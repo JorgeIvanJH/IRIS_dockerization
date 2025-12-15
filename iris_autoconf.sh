f552934de2d451cc6675fc68de22673ed5241bdf
JorgeIvanJH <jorgeivan.jaramillo.herrera@gmail.com>
2025-12-15 11:58:34 +0000

    first working automatic table population after build and container run

--- iris_autoconf.sh
+++ iris_autoconf.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 set -e
 
-# Run any extra initialization *after* IRIS is already running
 iris session IRIS <<'EOF'
-write "PRINT THIS: SEE MEEE HERE"
+write "PRINT THIS: SEE MEEE HERE",!
+
+/* Install IPM/ZPM client if you still need that first
+   (your original snippet did this already) */
 s version="latest" s r=##class(%Net.HttpRequest).%New(),r.Server="pm.community.intersystems.com",r.SSLConfiguration="ISC.FeatureTracker.SSL.Config" d r.Get("/packages/zpm/"_version_"/installer"),$system.OBJ.LoadStream(r.HttpResponse.Data,"c")
-q
+
+/* Configure registry */
+zpm
+repo -r -n registry -url https://pm.community.intersystems.com/ -user "" -pass ""
+install csvgenpy
+quit
+
+/* Upload csv data to Table Automatically using csvgenpy */
+do ##class(shvarov.csvgenpy.csv).Generate("/dur/mocktable.csv","MockPackage","MockTable")
+
 halt
 EOF
